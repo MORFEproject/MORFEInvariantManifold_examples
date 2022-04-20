8b4ead48f1746fae226f49b1341a1a2c2de475fb
MORFEproject <100767088+MORFEproject@users.noreply.github.com>
2022-04-20 11:58:07 +0100

    Update and rename cantilever_hex_autonomous_undamped.jl to cantilever_autonomous_undamped.jl

--- script/cantilever_autonomous_undamped.jl
+++ script/cantilever_autonomous_undamped.jl
@@ -1,11 +1,10 @@
 # Import the module. Using "using" keyword to make all export functions accessible.
 using MORFEInvariantManifold
+using MATLAB
 
 # Name of the mesh file. The one of this example is a COMSOL mesh format.
+# Meshes associated to the blade are used to benchmark the scalability of the code.
 mesh_file = "cantilever_hexa.mphtxt"
-
-# add material
-MORFE_add_material("Titanium",4400.0,104e9,0.3)
 
 ### DOMAINS INFO
 # domain_list is a vector that stores vectors of integers. 
@@ -13,6 +12,17 @@ MORFE_add_material("Titanium",4400.0,104e9,0.3)
 domains_list = [                 
 [1]
 ]
+
+### MATERIAL 
+# define material properties
+material_name = "Titanium";
+density = 4400.0;
+young_modulus = 104e9;
+poisson_ratio = 0.3;
+# add material to the library. 
+# Once added is saved and can be used without redefining it
+MORFE_add_material(material_name,density,young_modulus,poisson_ratio)
+# assign materials to the domain:
 # materials is an array of strings. 
 # materials[i] embeds the material associated to the domain defined in domains_list[j]
 materials = [
@@ -52,23 +62,53 @@ bc_vals = [
 # style: parametrisation style. 'g' graph, 'r' real normal form, 'c' complex normal form
 style = 'c'
 # max_order: maximum order of the asymptotic expansion of the autonomous problem
-max_order = 5
+max_order = 9
 
 odir, Cp, rdyn = MORFE_mech_autonomous(mesh_file,domains_list,materials,
                                        boundaries_list,constrained_dof,bc_vals,
                                        α,β,
                                        Φₗᵢₛₜ,style,max_order);
 
+# post proc
+ω₀ = imag(Cp[2].f[1,1]) # extract eigenfrequency
+T₀ = 2*pi /ω₀  # extract period
+#
+L = 1.0   # characteristic length of the vibration amplitude
+N = size(Cp[2].W)[1]; # number of physical DOFs 
+ϕ = maximum(abs.(Cp[2].W[Int(N/2)+1:N,1])) # maximum value of the mode
 
-x0 = [0.0,0.5]
-integration_length = 3.0
+small_amplitude = [0.0,0.01]
+time_integration_length = 2*T₀
 forward = true
-MaxNumPoints = 100
+MaxNumPoints = 40
 minstep = 1e-8
-maxstep = 20.0
+maxstep = 0.5
+initstep = 0.1
 ncol = 4.0
 ntst = 40.0
-MORFE_integrate_rdyn_backbone(odir,x0,integration_length,forward,MaxNumPoints,minstep,maxstep,ncol,ntst)
 
-
+ms = MORFE_integrate_rdyn_backbone(odir,small_amplitude,
+                                    time_integration_length,forward,MaxNumPoints,
+                                    minstep,maxstep,initstep,ncol,ntst)
+#
 frf = MORFE_compute_backbone_modal(odir)
+
+
+x = frf[:,1]./ω₀
+y = frf[:,2].*(ϕ/L)
+# plot results
+put_variable(ms,:x,x)
+put_variable(ms,:y,y)
+put_variable(ms,:max_order,max_order)
+
+show_msession(ms) # do not close the pop up matlab windows until done with the analyses
+eval_string(ms,"
+figure(1);hold on
+plot(x,y,'DisplayName',strcat(\"Order \",num2str(max_order)))
+xlim([0.995,1.025])
+ylim([0.,1.2])
+xlabel('\$\\omega/\\omega_1\$','Interpreter','latex');
+ylabel('max[\$u_1 \\phi_1\$]/\$L\$','Interpreter','latex');
+legend()
+")
+close(ms)
